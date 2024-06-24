1effeb3c9b3a04a77e5bc9c157f163e5ac0e0f4a
Hernan <hernan.alejo.2001@gmail.com>
2024-06-24 17:51:05 -0500

    Ejercicios de tipos de variables y creacion de funciones basicas

---
+++ labVariables.js
@@ -0,0 +1,10 @@
+function contact(name, phone, email){
+    let nombre =  name;
+    let cell = phone;
+    let correo = email;
+    console.log(nombre,cell,correo);
+}
+
+contact("Juan", "09 45744","emailPedro.com");
+contact("Maxwell", "097457","email.com");
+contact("Pedro", "09 45744","emailPedro.com");
--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 console.log("Hello, World first Messagge!");
+//let height; // no permite declarar otra variable con ese nombre 
+//console.log(height);  //  ->  undefined
+//console.log(weight);  //  ->  Uncaught  ReferenceError:  weight  is  not  defined
+
+let  steps  =  100; 
+console.log(steps);  //  ->  100
+steps  =  120;  //  ->  120
+console.log(steps);
+steps  =  steps  +  200;
+console.log(steps);  //  ->  320
+
+
+let  height  =  180;
+{
+         let  weight  =  70;
+         console.log(height);  //  ->  180
+         console.log(weight);  //  ->  70	
+}
+console.log(height);  //  ->  180
+//console.log(weight);  //  ->  Uncaught  ReferenceError:  weight  is  not  defined
+
+var  globalGreeting  =  "Good  ";
+   
+function  testFunction()  {
+         var  localGreeting  =  "Morning  ";    
+         console.log("function:");
+         console.log(globalGreeting);
+         console.log(localGreeting);
+}
+   
+testFunction();
+   
+console.log("main  program:");
+console.log(globalGreeting);
+console.log(localGreeting);  //  ->  Uncaught  ReferenceError:  localGreeting  is  not  defined
