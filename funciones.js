7b2b4c0eda021a3e2605bc337dc9b0e38cdc04d8
Hernan <hernan.alejo.2001@gmail.com>
2024-06-30 23:26:53 -0500

    Ejercicios con funciones

---
+++ funciones.js
@@ -0,0 +1,29 @@
+{
+    let inner = function() {
+        console.log('inner 1');
+   }
+   let outer = function(callback) {
+        console.log('outer 1');
+        callback();
+        console.log('outer 2');
+   }
+   console.log('test 1');
+   outer(inner);
+   console.log('test 2');
+}
+
+{
+    //Devoliciones de llamadas asincronas
+    let inner = function() {
+        console.log('inner 1');
+        }
+        let outer = function(callback) {
+        console.log('outer 1');
+        setTimeout(callback, 1000) /*ms*/;
+        console.log('outer 2');
+        }
+        console.log('test 1');
+        outer(inner);
+        console.log('test 2');
+        
+}
